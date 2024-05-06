a11c6373a14629e891add9c53089477a6a2f7a15
jeetisngh-621 <jeetsinghjeet68@gmail.com>
2024-05-06 13:13:41 +0530

    frame name update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import { useGSAP } from '@gsap/react';
 import gsap from 'gsap';
 import React from 'react'
 import { FaArrowRightLong } from "react-icons/fa6";
-import frame from '../../public/Images/frame.png'
+import frame from '../../public/Images/Frame.png'
 
 
 function Hero() {
